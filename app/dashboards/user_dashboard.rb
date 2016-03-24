f40089bd5e10b3584b9e4990ab1f65856ad3d0ae
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-24 16:21:03 -0500

    done

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -10,15 +10,7 @@ class UserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     email: Field::String,
-    encrypted_password: Field::String,
-    reset_password_token: Field::String,
-    reset_password_sent_at: Field::DateTime,
-    remember_created_at: Field::DateTime,
-    sign_in_count: Field::Number,
-    current_sign_in_at: Field::DateTime,
-    last_sign_in_at: Field::DateTime,
-    current_sign_in_ip: Field::String.with_options(searchable: false),
-    last_sign_in_ip: Field::String.with_options(searchable: false),
+    password: Field::String,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }
@@ -31,8 +23,6 @@ class UserDashboard < Administrate::BaseDashboard
   COLLECTION_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
   ]
 
   # SHOW_PAGE_ATTRIBUTES
@@ -40,15 +30,6 @@ class UserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
     :created_at,
     :updated_at,
   ]
@@ -58,15 +39,7 @@ class UserDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    # :reset_password_sent_at,
-    # :remember_created_at,
-    # :sign_in_count,
-    # :current_sign_in_at,
-    # :last_sign_in_at,
-    # :current_sign_in_ip,
-    # :last_sign_in_ip,
+    :password,
   ]
 
   # Overwrite this method to customize how users are displayed
