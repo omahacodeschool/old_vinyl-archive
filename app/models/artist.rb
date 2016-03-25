3d1223645fb83af3249e829e9b91438e61a08a0a
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-25 17:01:47 -0500

    going to do more edits to this later but both artist show/index are working

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,14 @@
 class Artist < ActiveRecord::Base
   belongs_to :area
   has_many :members
+
+  def show_artist_image
+    x = self.id
+    return ArtistImage.where({"artist_id" => x})
+  end
+
+  def show_artist_members
+    x = self.id
+    return Member.where({"artist_id" => x})
+  end
 end
