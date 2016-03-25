a85358d6a0062c5560c29c6255c85ca7f272dd63
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-25 11:50:47 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+areas = Area.create([
+  { name: 'Omaha' }, 
+  { name: 'Lincoln' }
+  { name: 'Grand Island' }
+  { name: 'Kearney' }
+  { name: 'Columbus' }
+  { name: 'North Platte' }
+  { name: 'Scottsbluff' }
+  { name: 'McCook' }
+  { name: 'Bellevue' }
+  { name: 'Fremont' }
+  { name: 'Hastings' }
+  { name: 'Norfolk' }
+  { name: 'York' }
+  { name: 'Lexington' }
+  { name: 'Wayne' }
+  { name: 'Broken Bow' }
+  { name: 'Valentine' }
+  { name: 'Ord' }
+  { name: 'Imperial' }
+  ])
+
+formats = Format.create([
+  { name: 'LP' }, 
+  { name: 'EP' }
+  { name: 'Single' }
+  { name: '7-Inch Single' }
+  { name: '10-Inch Single' }
+  { name: '12-Inch Single' }
+  ])
+
+genres = Genre.create([
+  { name: 'Rock' }, 
+  { name: 'Pop' }
+  { name: 'Rap/Hip Hop' }
+  { name: 'Soul/R&B' }
+  { name: 'Punk' }
+  { name: 'Ska' }
+  { name: 'Folk' }
+  { name: 'Metal' }
+  { name: 'Bluegrass' }
+  { name: 'Country' }
+  { name: 'Electronic' }
+  ])
+
+instruments = Instrument.create([
+  { name: 'Guitar' }, 
+  { name: 'Vocals' }
+  { name: 'Bass' }
+  { name: 'Drums' }
+  { name: 'Keyboards' }
+  { name: 'Violin' }
+  { name: 'Banjo' }
+  { name: 'Trumpet' }
+  { name: 'Sax' }
+  { name: 'Multi-instrumentalist' }
+  { name: 'Other' }
+  ])
