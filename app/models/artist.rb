dfe68b1e402a6858f5ecb101cea3602a4bc2a305
Chris Foral <chris.foral@gmail.com>
2016-03-28 09:46:23 -0500

    'count_by_decade' method defined to collect artists associated with any given decade back to 1860 and will update for each coming decade. I believe all info necessary for homepage is now collected in the View.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,10 +2,16 @@ class Artist < ActiveRecord::Base
   belongs_to :area
   has_many :members
 
-  # Prints decade and count of artists associated with that decade
+  # Finds count of artists associated with a given decade and returns a hash with decade keys and artist count values
   def self.count_by_decade
     decade = Time.now.year - (Time.now.year % 10)
+    decade_artists = {}
 
-    self.where("start_year >= ? AND start_year <= ?", decade, (decade + 10)).count
+    while decade >= 1860 do
+      decade_artists[decade] = self.where("start_year >= ? AND start_year <= ?", decade, (decade + 10)).count
+
+      decade -= 10
+    end
+    decade_artists
   end
 end
