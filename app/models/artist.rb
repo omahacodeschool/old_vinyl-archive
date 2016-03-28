8fae6d799e8a797f69502b494dc407973a0ec025
Chris Foral <chris.foral@gmail.com>
2016-03-27 21:33:42 -0500

    Getting closer method for method identifying the count of artists for a given decade

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,9 +3,9 @@ class Artist < ActiveRecord::Base
   has_many :members
 
   # Prints decade and count of artists associated with that decade
-  def count_by_decade
+  def self.count_by_decade
     decade = Time.now.year - (Time.now.year % 10)
 
-    self.where("start_year >= ? AND start_year <= ?", decade, (decade + 10))
+    self.where("start_year >= ? AND start_year <= ?", decade, (decade + 10)).count
   end
 end
