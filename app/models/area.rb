a66e8433764b792668a9c87f271664282f28e27e
Chris Foral <chris.foral@gmail.com>
2016-03-28 13:26:31 -0500

    Done for Areas index page

--- app/models/area.rb
+++ app/models/area.rb
@@ -6,8 +6,9 @@ class Area < ActiveRecord::Base
     Artist.where({"area_id" => self.id}).count
   end
 
-  # Return artists associated with a given area
-  def with_these_artists
-    Artist.where({"area_id" => self.id})
+  # Return albums associated with a given area
+  def with_these_albums
+    artist_id = Artist.where({"area_id" => self.id})
+    Album.where({"id" => artist_id})
   end
 end
