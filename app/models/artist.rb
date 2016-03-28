a0a65bec9409a3e5264f4870b1f2363a643ee298
Chris Foral <chris.foral@gmail.com>
2016-03-27 20:39:20 -0500

    Defining method to collect artists associated with any given decade

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,11 @@
 class Artist < ActiveRecord::Base
   belongs_to :area
   has_many :members
+
+  # Prints decade and count of artists associated with that decade
+  def count_by_decade
+    decade = Time.now.year - (Time.now.year % 10)
+
+    self.where("start_year >= ? AND start_year <= ?", decade, (decade + 10))
+  end
 end
