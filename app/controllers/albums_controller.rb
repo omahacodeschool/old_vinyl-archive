27baf65ae5fc6720a40165d559ae1a03bd707e66
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-28 23:36:37 -0500

    WIP

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,11 @@
 class AlbumsController < ApplicationController
   def show
     @album = Album.find_by_id(params[:id])
-    @artist = Artist.find_by_id(params[:id])
-    @release = Release.find_by_id(params[:id])
-    @format = Format.find_by_id(params[:id])
-    @area = Area.find_by_id(params[:id])
-    @genre = Genre.find_by_id(params[:id])
+    @artist = Artist.all
+    @release = Release.all
+    @format = Format.all
+    @area = Area.all
+    @genre = Genre.all
   end
 
   def index
--- app/models/album.rb
+++ app/models/album.rb
@@ -3,19 +3,40 @@ class Album < ActiveRecord::Base
   belongs_to :artist
   belongs_to :release
 
-  # Return the AlbumImage object for this album.
+  def show_artist_name
+    x = Album.find_by_artist_id(self.id)
+    Artist.where({"id" => x}).name       #Shows "Artist" instead of name
+  end
+
+  # Return the AlbumImage object for this album
   def album_image
     AlbumImage.find_by_album_id(self.id)
   end
 
+  # Shows all tracks for album
   def show_album_tracks
     x = self.id
     Track.where({"album_id" => x})
   end
 
+  def show_release_date
+    Release.find_by_id(self.id).release_date
+  end
+
+  def show_album_area
+    # Album --> Artist --> Area
+  end
+
+  def show_album_genre
+    Album.find_by_id(self.id).genre_id #Need genre name to show
+  end
+
+  def show_album_label
+    Release.find_by_id(self.id).label
+  end
+
   def show_album_format
-    x = self.id
-    Format.where({"album_id" => x})
+    Release.find_by_id(self.id).format_id #Need format name to show
   end
 
 end
