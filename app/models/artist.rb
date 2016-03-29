3ea460a96ddf2afbbc850c352d1e5158b28a7a5c
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-29 13:52:51 -0500

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,9 @@ class Artist < ActiveRecord::Base
   belongs_to :area
   has_many   :members
   has_many   :albums
+  has_many   :album_images
   has_many   :artist_images
+  has_many   :genres, through: :albums
 
   # Lists all members for a particular band
   def show_artist_members
