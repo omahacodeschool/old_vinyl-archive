2a59e1a7c863e654fdb8149b13fa191465ea2879
Chris Foral <chris.foral@gmail.com>
2016-03-29 15:27:05 -0500

    Renamed the link methods for consistent naming with routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,33 @@
 module ApplicationHelper
   # ????
-  def album_index_link(album)
+  def albums_index_link(album)
     link_to(Albums, "/albums/index")
   end
 
-  def album_show_link(album)
+  def albums_show_link(album)
     link_to(album.title, "/albums/#{album.id}")
   end
 
-  def artist_show_link(artist)
+  def artists_show_link(artist)
     link_to(artist.name, "/artists/#{artist.id}")
   end
 
   # ????
-  def area_index_link(area)
+  def areas_index_link(area)
     link_to(Areas, "/areas/index")
   end
 
-  def area_show_link(area)
+  def areas_show_link(area)
     link_to(area.name, "/areas/#{area.id}")
   end
 
-  def genre_show_link(genre)
+  def genres_show_link(genre)
     link_to(genre.name, "/genres/#{genre.id}")
   end
 
-  def format_show_link(format)
+  def formats_show_link(format)
     link_to(format.name, "/formats/#{format.id}")
   end
 
-  
+
 end
