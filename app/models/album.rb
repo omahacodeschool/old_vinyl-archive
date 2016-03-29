d2e27dfda8ee20ad568534fe6faffc5a26d55c23
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-29 11:03:29 -0500

    format still says Format...

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,57 +1,41 @@
 class Album < ActiveRecord::Base
   belongs_to :genre
   belongs_to :artist
-  belongs_to :release
+  has_many   :releases
   has_many   :album_images
   has_many   :tracks
+  has_one    :area, through: :artist
+  has_many   :formats, through: :releases
 
-  def show_artist_name
-    x = Album.find_by_artist_id(self.id)
-    Artist.where({"id" => x}).name       #Shows "Artist" instead of name
-  end
-
-  # Return the AlbumImage object for this album
+  # Return the AlbumImage object for this album.
   def album_image
     AlbumImage.find_by_album_id(self.id)
   end
+
   # Retrieve tracks associated with a given album and return hash with track number keys and track title values
-  # NOT YET WORKING
-  def self.tracks
-    album_tracks = Track.where({"album_id" => self.id})
+  def tracklist
+    tracks = Track.where({"album_id" => self.id})
     tracklist = {}
     track_number = 1
 
-  # Shows all tracks for album
-  def show_album_tracks
-    x = self.id
-    Track.where({"album_id" => x})
+    tracks.each do |track|
+      tracklist[track_number] = track.title
+      track_number += 1
+    end
+    tracklist
   end
 
   def show_release_date
     Release.find_by_id(self.id).release_date
   end
 
-  def show_album_area
-    # Album --> Artist --> Area
-  end
-
-  def show_album_genre
-    Album.find_by_id(self.id).genre_id #Need genre name to show
-  end
-
   def show_album_label
     Release.find_by_id(self.id).label
   end
-
+  
   def show_album_format
-    Release.find_by_id(self.id).format_id #Need format name to show
-  end
-
-    while track_number <= album_tracks.count
-     tracklist[track_number] = album_tracks.title
-     track_number += 1
-    end
-    tracklist
+    x = self.id
+    Format.where({"album_id" => x})
   end
 
 end
