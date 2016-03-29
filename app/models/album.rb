3e750330a832232808310777848b0509f6a0c7a6
Chris Foral <chris.foral@gmail.com>
2016-03-28 22:23:41 -0500

    Removed more unused or near-identical methods

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,8 @@ class Album < ActiveRecord::Base
   belongs_to :genre
   belongs_to :artist
   belongs_to :release
+  has_many   :album_images
+  has_many   :tracks
 
   # Return the AlbumImage object for this album.
   def album_image
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,8 @@
 class Artist < ActiveRecord::Base
   belongs_to :area
-  has_many :members
-  has_many :albums
+  has_many   :members
+  has_many   :albums
+  has_many   :artist_images
 
   # Lists all members for a particular band
   def show_artist_members
@@ -14,6 +15,5 @@ class Artist < ActiveRecord::Base
     x = self.id
     Album.where({"artist_id" => x})
   end
-
 end
 
