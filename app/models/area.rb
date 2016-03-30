81f371bfcba120f98bd8de579cb3e81abf5142c0
Chris Foral <chris.foral@gmail.com>
2016-03-30 12:46:50 -0500

    Fixed Area 'with_these_albums' method

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,9 +1,13 @@
 class Area < ActiveRecord::Base
   has_many :artists
+  has_many :albums, through: :artists
 
   # Return albums associated with a given area
   def with_these_albums
-    artist_id = Artist.where({"area_id" => self.id})
-    Album.where({"id" => artist_id})
+    artist_ids = Artist.where({"area_id" => self.id})
+    artist_ids.each do |artist|
+      albums = Album.where({"artist_id" => artist})
+    end
+    albums
   end
 end
