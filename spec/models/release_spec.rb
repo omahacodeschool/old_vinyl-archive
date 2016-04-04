d53ea2b9132cb65b6a79c0deaed9b87f50919c90
Chris Foral <chris.foral@gmail.com>
2016-04-03 23:55:13 -0500

    Basic layout, sans assertions, for release model test

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Release, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "::by_decade" do
+    context "when decades have album release's release dates within them" do
+      it "returns a hash of decades and Release collections" do
+        # place assertions here
+      end
+    end
+  end
+
 end
