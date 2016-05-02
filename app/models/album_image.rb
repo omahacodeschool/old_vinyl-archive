bd44e2e4cebd348d04efc57a2dfa338c4e14fc28
Sumeet Jain <sumeet@sumeetjain.com>
2016-05-02 08:41:39 -0500

    Mount uploaders

--- app/models/album_image.rb
+++ app/models/album_image.rb
@@ -1,4 +1,11 @@
 class AlbumImage < ActiveRecord::Base
   belongs_to :album
 
+  mount_uploader :cover_image, ImageUploader
+  mount_uploader :backcover_image, ImageUploader
+  mount_uploader :insert_image_1, ImageUploader
+  mount_uploader :insert_image_2, ImageUploader
+  mount_uploader :insert_image_3, ImageUploader
+  mount_uploader :record_image, ImageUploader
+
 end
--- app/models/artist_image.rb
+++ app/models/artist_image.rb
@@ -2,4 +2,8 @@ class ArtistImage < ActiveRecord::Base
   belongs_to :artist
 
   mount_uploader :image_1, ImageUploader
+  mount_uploader :image_2, ImageUploader
+  mount_uploader :image_3, ImageUploader
+  mount_uploader :image_4, ImageUploader
+  mount_uploader :image_5, ImageUploader
 end
